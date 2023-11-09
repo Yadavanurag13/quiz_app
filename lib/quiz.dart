c0e40d08ccfe5dd5667fc7ed1fad969ad13f7b36
Anurag Yadav <yadavanurag1310@gmail.com>
2023-11-10 00:19:30 +0530

    third commt

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/question_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -9,6 +10,13 @@ class Quiz extends StatefulWidget {
   }
 }
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
   @override
   Widget build(context) {
     return MaterialApp(
@@ -24,7 +32,7 @@ class _QuizState extends State<Quiz> {
                 end: Alignment.bottomRight
             ),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
